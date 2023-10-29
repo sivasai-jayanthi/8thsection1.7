93b605a2e718f7dc6b14f051f5aaa4e45bfc6706
sivasai-jayanthi <sai89938@gmail.com>
2023-10-30 00:30:46 +0530

    Add project files.

---
+++ 8thsection1.7/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _8thsection1._7
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			runApp();
+			Console.ReadLine();
+		}
+
+		public static void runApp()
+		{
+			SingleLinkedList list = new SingleLinkedList();
+			Node n = new Node();
+			n.Data = "root";
+			list.add(n);
+			for (int i = 0; i < 10; i++)
+			{
+				Node node = new Node();
+				node.Data = Convert.ToString(i);
+				list.add(node);
+			}
+
+			Console.WriteLine("length of singly linked list=" + list.getLengthOfList());
+
+			n = list.getRoot();
+			while (n != null)
+			{
+				Console.WriteLine(n.Data);
+				n = list.getNext();
+			}
+		}
+	}
+}
---
+++ 8thsection1.7/node.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _8thsection1._7
+{
+	public class Node
+	{
+		private string data;
+		private Node next = null;
+		public string Data
+		{
+			get { return this.data; }
+			set { this.data = value; }
+		}
+
+		public Node Next
+		{
+			get { return this.next; }
+			set { this.next = value; }
+		}
+	}
+}
